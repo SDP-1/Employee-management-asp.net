edae0bd3e620c37d714c3e655b95b2134bf5f587
Devinda P S <98279413+SDP-1@users.noreply.github.com>
2024-11-14 10:04:10 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Employee_management_asp.net.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
